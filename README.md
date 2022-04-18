9975944d374e93ad413709761bf1ec9822902d99
Random Guy <leekhoi5319990@gmail.com>
2022-04-18 15:05:59 +0700

    05-x.4_Colocating tests

--- README.md
+++ README.md
@@ -28,3 +28,5 @@
 ## Mocking HTTP requests
 - Use `msw` ([at 01:30](https://epicreact.dev/modules/testing-react-apps/mocking-http-requests-solution-1)) as a request interceptor for testing (so that we don't have to worry about finding an available port for the server to listen to and making sure we're making requests to the right port).
 - Use `msw` as a offline [module](https://epicreact.dev/modules/testing-react-apps/mocking-http-requests-extra-credit-solution-1) to write UI for APIs that aren't finished yet.
+- Use [`toMatchInlineSnapshot()`](https://epicreact.dev/modules/testing-react-apps/mocking-http-requests-extra-credit-solution-3) rather than an explicit assertion on an error element to keep your tests up-to-date if the error message were to change in the future.
+- Colocating run-time server behavior tests (at [0:30](https://epicreact.dev/modules/testing-react-apps/mocking-http-requests-extra-credit-solution-4)) by using `server.use`. For such cases, remember to add `server.resetHandlers()` to preserve test isolation and restore the original handlers for other tests.
--- src/__tests__/final/TS/05.tsx
+++ src/__tests__/final/TS/05.tsx
@@ -7,6 +7,7 @@ import userEvent from '@testing-library/user-event'
 import {build, fake} from '@jackfranklin/test-data-bot'
 import {setupServer} from 'msw/node'
 import {handlers} from 'test/server-handlers'
+import {rest} from 'msw'
 import Login from '../../../components/login-submission'
 import {LoginFormValues} from '../../../components/login'
 
@@ -34,4 +35,56 @@ test(`logging in displays the user's username`, async () => {
 	await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
 
 	expect(screen.getByText(username)).toBeInTheDocument()
+})
+
+test('omitting the username results in an error', async () => {
+	render(<Login />)
+	const {password} = buildLoginForm()
+
+	userEvent.type(screen.getByLabelText(/password/i), password)
+	// didn't type in the username and then submit
+	userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+	await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+	// error message is returned from the 'src\test\server-handlers.ts'
+	expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(
+		`"username required"`,
+	)
+})
+
+test('omitting the password results in an error', async () => {
+	render(<Login />)
+	const {username} = buildLoginForm()
+
+	userEvent.type(screen.getByLabelText(/username/i), username)
+	// didn't type in the password and then submit
+	userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+	await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+	expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(
+		`"password required"`,
+	)
+})
+
+test('unknown server error displays the error message', async () => {
+	const testErrorMessage = "Shit's fucked"
+	server.use(
+		rest.post(
+			'https://auth-provider.example.com/api/login',
+			async (_req, res, ctx) => {
+				return res(
+					ctx.status(500),
+					ctx.json({message: testErrorMessage}),
+				)
+			},
+		),
+	)
+	render(<Login />)
+	userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+	await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+	expect(screen.getByRole('alert')).toHaveTextContent(testErrorMessage)
 })
